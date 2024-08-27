80edee4eee9b345142e5fc7f6b602070c110d8ed
pranjali maletha <126933724+PRANJALIMALETHA@users.noreply.github.com>
2024-08-27 17:50:23 +0530

    Create exports--node.js

---
+++ exports--node.js
@@ -0,0 +1,33 @@
+export module in same diirectory---
+module.export
+
+
+require---->a built in function that include external module that exist in sperate file
+
+
+module.export ----> aspecial object
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+./---- same directory file is used it means
+
+
+
+By defualt module.exports send an empty object
+...so explicity we can make it no as well as string whatever we want
+
+
+script.js(file)
+
+const somvalue=require("./math");
+console.log(somvalue);
+
+script.js(file)
+module.exports(math.js);
+
+
+\backened4kill> node script.js
+123
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
